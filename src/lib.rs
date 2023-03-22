03668ac8e15ccfcb13e5abe8ba107d66c90812d3
Samuel Joli <samuel.joli.ftn@gmail.com>
2023-03-22 15:09:09 -0400

    Refactor mod tree

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,10 @@ use axum::{
 use hyper::server::conn::AddrIncoming;
 use std::net::SocketAddr;
 
-async fn health_check() -> Result<(), ()> {
-    // Returns an empty response
-    Ok(())
-}
+mod routes;
 
 pub fn make_server(addr: &SocketAddr) -> Server<AddrIncoming, IntoMakeService<axum::Router>> {
-    let app = Router::new().route("/health_check", get(health_check));
+    let app = Router::new().route("/health_check", get(routes::get_health_check));
 
     axum::Server::bind(addr).serve(app.into_make_service())
 }
---
+++ src/routes/health_check.rs
@@ -0,0 +1,4 @@
+pub async fn get_health_check() -> Result<(), ()> {
+    // Returns an empty response
+    Ok(())
+}
