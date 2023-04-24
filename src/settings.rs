0f8cf118a1c79e6a5cb8f7613e1f728202f5ca34
Samuel Joli <samuel.joli.ftn@gmail.com>
2023-04-23 22:29:13 -0400

    Clean up comments

--- src/settings.rs
+++ src/settings.rs
@@ -10,21 +10,19 @@ pub struct Settings {
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        /*
-         * Hierarchial/Layered config composition.
-         * 1. Load from defaults configs.
-         * 2. Load from "env" based configs.
-         * 3. Load from ENVIRONMENT
-         */
         let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
 
+        //Hierarchial/Layered config composition.
         let s = Config::builder()
+            // Load from defaults configs.
             .add_source(File::with_name("config/default"))
+            // Load from "env" based configs.
             .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
+            // Load from ENVIRONMENT
             .add_source(Environment::default())
             .build()?;
 
-        // You can deserialize (and thus freeze) the entire configuration as
+        // Deserialize and freeze configs as is.
         s.try_deserialize()
     }
 }
